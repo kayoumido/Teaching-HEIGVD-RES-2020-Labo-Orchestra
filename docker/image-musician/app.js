e849927145700d121aeb2d0a0308dd4d47df55d9
Doran Kayoumi <dorankayoumi@gmail.com>
2020-06-05 17:02:35 +0200

    Add first version of musician

---
+++ docker/image-musician/app.js
@@ -0,0 +1,44 @@
+const uuid = require('uuid');
+const dgram = require('dgram');
+const moment = require('moment');
+
+const s = dgram.createSocket('udp4');
+
+const instruments = {
+  piano: 'ti-ta-ti',
+  trumpet: 'pouet',
+  flute: 'trulu',
+  violin: 'gzi-gzi',
+  drum: 'boum-boum',
+};
+
+if (process.argv.length - 2 != 1) {
+  console.error("Too many or too few arguments!");
+  return;
+}
+
+if (!instruments[process.argv[2]]) {
+  console.error("Unknown instrument!");
+  return;
+}
+
+const musician = {
+  uuid: uuid.v4(),
+  instrument: process.argv[2],
+  activeSince: new Date(),
+}
+
+setInterval(() => {
+  const message = Buffer.from(JSON.stringify({
+    uuid: musician.uuid,
+    sound: instruments[musician.instrument],
+    when: new Date(),
+    activeSince: musician.activeSince,
+  }));
+
+  s.send(message, 0, message.length, 3000, "239.255.22.5", (err, bytes) => {
+    console.log('Sending package yay');
+  });
+}, 1000);
+
+
