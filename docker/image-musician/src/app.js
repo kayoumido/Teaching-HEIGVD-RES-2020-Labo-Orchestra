cb5ba7a3ad4ee54e91bf3722048841f62e467803
Doran Kayoumi <dorankayoumi@gmail.com>
2020-06-24 14:59:46 +0200

    Remove 2 first elements of argv

--- docker/image-musician/src/app.js
+++ docker/image-musician/src/app.js
@@ -14,14 +14,16 @@ instruments.set('drum', 'boum-boum');
 const MULTICAST_PORT = 3000;
 const MULTICAST_ADDR = '239.255.22.5';
 
+const argv = process.argv.slice(2);
+
 // since process.argv also contains `node` and the path to the script,
 // we simply do - 2 to see have only 1 argument.
-if (process.argv.length - 2 != 1) {
+if (argv.length != 1) {
   console.error("Too many or too few arguments!");
   return;
 }
 
-if (!instruments.get(process.argv[2])) {
+if (!instruments.get(argv[0])) {
   console.error("Unknown instrument!");
   return;
 }
@@ -29,11 +31,13 @@ if (!instruments.get(process.argv[2])) {
 // build a musician object to keep his information
 const musician = {
   uuid: uuid.v4(),
-  instrument: process.argv[2],
+  instrument: argv[0],
   activeSince: moment().format(),
 }
 
 setInterval(() => {
+  console.log(musician);
+
   const message = Buffer.from(JSON.stringify({
     uuid: musician.uuid,
     sound: instruments.get(musician.instrument),
